26b4fc89888c411a761efc7af52d82212af4a41d
saggington <owenanjaya@gmail.com>
2025-07-29 22:05:23 +0700

    movement+mulaifishing

---
+++ MancingMania/Assets/Scripts/Fishing/MinigameManager.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI inputText;
+    private string text;
+
+
+    private void Update()
+    {
+        inputText.text = "BLABLABLA";
+    }
+
+    private void AddInput()
+    {
+
+    }
+}
---
+++ MancingMania/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float sens = 50f;
+
+    private float xRotation;
+    private float yRotation;
+
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // Horizontal input
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1f); // Optional: prevents faster diagonal movement
+
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+        }
+
+        //Mouse Rotation
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Combine horizontal and vertical movement
+        Vector3 finalMove = (move * playerSpeed) + (playerVelocity.y * Vector3.up);
+        controller.Move(finalMove * Time.deltaTime);
+    }
+
+}
